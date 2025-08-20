9216c060458b02245830d59667d663cd154125e3
Christian Castro <chrisvanklaus@gmail.com>
2025-08-19 19:22:47 -0500

    Función agregar amigos

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,1 +1,22 @@
 let amigos =[];
+
+function agregarAmigo()
+{
+    let nuevoAmigo = document.getElementById('amigo').value;
+
+    if (nuevoAmigo == '')
+    {
+        alert ('Agregue un nombre valido')
+    }
+    else {
+    amigos.push(nuevoAmigo);
+    console.log(amigos);
+    limpiarEntrada();
+    }
+}
+
+function limpiarEntrada()
+{
+  let valorEntrada = document.getElementById('amigo')
+  valorEntrada.value = '';
+} 
